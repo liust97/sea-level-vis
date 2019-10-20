bc255feaf09afae48f3afb8067d24b21b6dc8282
Sitong Liu <liusit@foxmail.com>
2019-10-20 10:27:52 -0700

    Update App.js parse float

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";  
-import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
 import "./App.css";
-import Routes from "./Routes";
 import Ocean from "./components/Ocean";
+import Thermometer from "./components/Thermometer";
 import { Slider } from 'antd';
 
 const data = require('./static/nasa_data.json');
@@ -14,27 +12,28 @@ class App extends React.Component {
     super(props);
     this.state = {
       year: initialYear,
-      co2: parseInt(data[initialYear]["co2"]),
-      temp: parseInt(data[initialYear]["temp"]),
-      arctic: parseInt(data[initialYear]["arctic"]),
-      sealevel: parseInt(data[initialYear]["sealevel"]),
-      greenland: parseInt(data[initialYear]["greenland"])
+      co2: parseFloat(data[initialYear]["co2"]),
+      temp: parseFloat(data[initialYear]["temp"]),
+      arctic: parseFloat(data[initialYear]["arctic"]),
+      sealevel: parseFloat(data[initialYear]["sealevel"]),
+      greenland: parseFloat(data[initialYear]["greenland"])
     };
     this.setBackgroundColor(initialYear);
     this.onYearChange.bind(this);
     console.log(data)
   }
   setBackgroundColor(value) {
-    const bcolor = `hsl(197, 71%, ${430-this.state.co2}%)`;
+    // h from 200 to 360, 
+    const bcolor = `hsl(${26.4-80.6*this.state.temp}, 100%, 67%)`;
     document.getElementsByTagName('body')[0].style.backgroundColor = bcolor
   }
   onYearChange = value => {
     this.setState({year: value,
-      co2: parseInt(data[value]["co2"]),
-      temp: parseInt(data[value]["temp"]),
-      arctic: parseInt(data[value]["arctic"]),
-      sealevel: parseInt(data[value]["sealevel"]),
-      greenland: parseInt(data[value]["greenland"])
+      co2: parseFloat(data[value]["co2"]),
+      temp: parseFloat(data[value]["temp"]),
+      arctic: parseFloat(data[value]["arctic"]),
+      sealevel: parseFloat(data[value]["sealevel"]),
+      greenland: parseFloat(data[value]["greenland"])
     });
     this.setBackgroundColor(value)
   }
@@ -49,7 +48,7 @@ class App extends React.Component {
           onChange={this.onYearChange}
           tooltipVisible
         />
-
+        <Thermometer temp={this.state.temp} />
         <p className="year">{this.state.year}</p>
         <p>{this.state.co2}</p>
         <Ocean sealevel={this.state.sealevel} />
